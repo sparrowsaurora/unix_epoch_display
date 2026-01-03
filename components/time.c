cbcca85dca22362edf817759da61ac7e64a88844
Sparrows <35826584+sparrowsaurora@users.noreply.github.com>
2026-01-03 21:31:58 +0800

    feat(time): unix timestamp displays to terminal and is accurate

---
+++ components/time.c
@@ -0,0 +1,55 @@
+/*******************************************************************
+ * 
+ * File Name: time.C
+ *
+ * Description: 
+ *   Returns the current unix time for startup and adjustments
+ *   [possible: log desync ammounts.]
+ * 
+ * Author: sparrowsaurora
+ * Date: 3/1/26
+ * 
+ * Limitations:
+ *   We have to manualally account for the delay due to flashing the microcontroller
+ *   Hence `#define FLASH_TIME 9;' 
+ *
+ *******************************************************************/
+
+#include <time.h>
+#include <sys/time.h>
+#include <inttypes.h>
+#include <stdlib.h>
+
+#define FLASH_TIME 9; // seconds to flash microcontroller
+
+void set_time_from_build(void)
+{
+    setenv("TZ", "UTC-8", 1);
+    tzset();
+    struct tm tm = {0};
+
+    // Build time macros provided by compiler
+    strptime(__DATE__ " " __TIME__, "%b %d %Y %H:%M:%S", &tm);
+
+    time_t t = mktime(&tm);
+
+    struct timeval tv = {
+        .tv_sec = t,
+        .tv_usec = 0
+    };
+
+    settimeofday(&tv, NULL);
+}
+
+time_t getUnixTime(void) {
+    set_time_from_build();
+
+    time_t now;
+    time(&now);
+
+    // adjust to account for flashing time (6s - 15s)
+    now = now + FLASH_TIME; 
+
+    // printf("from build: %lld\n", now);
+    return now;
+}
--- main/unix_epoch_display.c
+++ main/unix_epoch_display.c
@@ -3,12 +3,18 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 
+// components
+#include "../components/time.c"
+#include "../components/display.c"
+#include "../components/rtc.c"
+
 void app_main(void)
 {
-    int i=0;
+    time_t time = getUnixTime();
+    // int i=0;
     while (1) {
-        printf("Unix epoch display alive: %d\n", i);
-        i++;
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        printf("Unix time: %lld\n", time); //time display reqs lld as it is a long long int
+        time++;
+        vTaskDelay(pdMS_TO_TICKS(1000)); // delay 1 second
     }
 }
